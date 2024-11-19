a3abc78c633422b4efed3caf2aa5c04bb7ac9209
Mercy <d00251752@student.dkit.ie>
2024-11-19 19:28:51 +0000

    add users and products models

---
+++ InventoryAPI/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryAPI.Models
+{
+	public class Products
+	{
+		public Guid productId { get; set; }
+
+		public string productName { get; set; }
+
+		public int quantity { get; set; }
+
+		public double price { get; set; }
+	}
+}
---
+++ InventoryAPI/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryAPI.Models
+{
+	public class Users
+	{
+		public Guid UsersId { get; set; }
+
+		[Required(ErrorMessage = "User name is required")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Range(0, 1)]
+		public int UserType { get; set; }
+
+	}
+}
